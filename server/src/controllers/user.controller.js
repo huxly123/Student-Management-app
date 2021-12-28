3c4633daa4823bbd1dcd87301356bf0605e0850d
huxly123 <huxlygerard5@gmail.com>
2021-12-28 18:01:29 +0530

    json

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -17,9 +17,16 @@ const authenticate = require("../middlewares/authenticate");
 // });
 
 router.get("/", async (req, res) => {
+
   try {
-    const data = await User.find();
-    return res.status(200).send(data);
+    const page = req.query.page || 1;
+    const size = req.query.size || 3;
+
+    const totalUserCount = await User.find().countDocuments().lean().exec();
+    const totalPages = Math.ceil(totalUserCount / size);
+    const offset = (page - 1) * size;
+    const user = await User.find().skip(offset).limit(size).lean().exec();
+    return res.status(200).send({user,totalPages})
   } catch (err) {
      console.log(err.message);
     return res.status(400).send(err.message);
